febd8385b4654a3d4e2a3da9474d76f4b3c63952
Giorgos Myrianthous <gmyrianthous1@sheffield.ac.uk>
2017-06-19 11:23:54 +0100

    structured perceptron for MultiArith dataset

---
+++ structured_perceptron.py
@@ -0,0 +1,48 @@
+import json 
+import re
+
+# Returns: list of dicts
+def read_data_json(filename):
+	print("Loading data..")
+	with open(filename) as data_file:    
+	    data = json.load(data_file)	
+	#print("\t-Number of training examples: " + str(len(data)))
+
+	return data
+
+# Report number of problems, sentences, words; vocab size; mean words and sentences per problem.
+def report_dataset_stats(data):
+	number_of_problems = len(data)
+	number_of_sentences = 0
+	number_of_words = 0
+	vocabulary = set([])
+
+	for i in range(0, len(data)):
+		number_of_sentences += len(re.split(r'[.!?]+', data[i]['sQuestion'])) - 1
+		words = re.findall(r'\w+', data[i]['sQuestion'])
+		for word in words:
+			vocabulary.add(word)
+
+		number_of_words += len(words)
+
+	vocab_size = len(vocabulary)
+	mean_words_per_problem = number_of_words / number_of_problems
+	mean_sentences_per_problem = number_of_sentences / number_of_problems
+
+	print("Dataset stats")
+	print("\t -Number of problems: " + str(number_of_problems))
+	print("\t -Number of sentences: " + str(number_of_sentences))
+	print("\t -Number of words: " + str(number_of_words))
+	print("\t -Vocabulary size: " + str(vocab_size))
+	print("\t -Mean words per problem: " + str(mean_words_per_problem))
+	print("\t -Mean sentences per problem: " + str(mean_sentences_per_problem) + "\n\n")
+
+
+
+if __name__ == "__main__":
+	# Read the data
+	data = read_data_json("data/MultiArith.json")
+	#pprint(data)
+
+	# Report dataset stats
+	report_dataset_stats(data)
