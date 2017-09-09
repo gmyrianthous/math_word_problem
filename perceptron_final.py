1a8b429548602378581c4d1f8fb6314335d46b5e
Giorgos Myrianthous <gmyrianthous1@sheffield.ac.uk>
2017-09-09 19:19:59 +0100

    final

--- perceptron_final.py
+++ perceptron_final.py
@@ -13,6 +13,11 @@ from nltk.tag.perceptron import PerceptronTagger
 from nltk.parse.stanford import StanfordDependencyParser
 import AllArithDataset
 import string
+import math
+import numpy
+import time
+from decimal import Decimal
+
 
 # Paths for NLTK models
 path_to_jar = 'stanford-corenlp-full-2017-06-09/stanford-corenlp-3.8.0.jar' 
@@ -61,7 +66,7 @@ def handle_command_line():
 		print("Please provide a dataset name (MultiArith, SingleOp, AllArith) and try again!")
 		sys.exit(1)
 	else:
-		if sys.argv[1] != "MultiArith" and sys.argv[1] != "SingleOp" and sys.argv[1] != 'AllArith':
+		if sys.argv[1] != "MultiArith" and sys.argv[1] != "SingleOp" and sys.argv[1] != 'AllArith' and sys.argv[1] != 'AllArithLex' and sys.argv[1] != 'AllArithTmpl' :
 			print("Please provide the correct dataset name!")
 			sys.exit(1)
 		else:
@@ -102,6 +107,12 @@ def k_fold_cross_validation(data, dataset):
 	elif dataset == "AllArith":
 		for i in range(0, 5):
 			k_folds.append(read_folds('data/allArith/fold'+str(i)+'.txt', dataset_indices))
+	elif dataset == "AllArithLex":
+		for i in range(0, 5):
+			k_folds.append(read_folds('data/allArithLex/fold'+str(i)+'.txt', dataset_indices))
+	elif dataset == "AllArithTmpl":	
+		for i in range(0, 5):
+			k_folds.append(read_folds('data/allArithTmpl/fold'+str(i)+'.txt', dataset_indices))		
 	return k_folds
 
 def get_numbers_in_template(alignment, question):
@@ -226,6 +237,14 @@ def extract_features(data, indices):
 			for i in range(0, len(operations)):
 				features['nextWord:'+str(next_words[i+1])+"_operation:"+operations[i]] += 1
 
+			#print(question)
+			#print(alignment)
+			# previous2words:word1_word2-operation:op
+			#previous2words = []
+			#for i in range(0, len(alignment)):
+
+			#sys.exit(1)
+
 			# Feature 3: eachFlag:True/False_operation:op
 			if ' each' in question:
 				features['questionContainsEach:True_lastOperation:'+operations[-1]] += 1
@@ -235,8 +254,8 @@ def extract_features(data, indices):
 				features['operation:'+operations[i]+"_positionInTemplate:"+str(i+1)] += 1
 
 			# Feature 5: secondOperation in template
-			if len(operations) > 1:
-				features['secondOperation:'+operations[1]] += 1
+			#if len(operations) > 1:
+			#	features['secondOperation:'+operations[1]] += 1
 
 			# Feature 6: asksHowMany:True/False_resultType:int/float
 			if 'how many' in question:
@@ -264,7 +283,7 @@ def extract_features(data, indices):
 
 
 			# Add the features into the dictionary
-			id_features_dict[index] = {}
+			id_features_dict[index] = OrderedDict()
 			id_features_dict[index]['features'] = features
 
 			# Also add some info that will be useful for the next actions
@@ -293,6 +312,8 @@ def initialise_weights(data):
 # Function for shuffling an OrderedDict
 def shuffle_data(data):
     # Shuffle the keys
+    #random.seed(146)
+
     keys = list(data.keys())
     random.shuffle(keys)
 
@@ -324,7 +345,7 @@ def get_equation_combinations(dataset_name, numbers):
 		    for ops in itertools.product(op, repeat=1):
 		        combinations.append("%s %s %s" % (
 		            symbols[0], ops[0], symbols[1]))
-	elif dataset_name == "AllArith":
+	elif dataset_name.startswith("AllArith"):
 		# Unknown number of parameters
 		symbol = list(string.ascii_lowercase[:len(numbers)])	
 		op = ['+', '-', '/', '*']
@@ -378,18 +399,30 @@ def extract_combination_features(problem, combination):
 	if ' each' in question:
 		features['questionContainsEach:True_lastOperation:'+operations[-1]] += 1
 	
+
+	#previous2words = problem['previous2words']
+
+	#features['previous2Word:'+str(previous2words[0])+"_operation:+"] += 1
+	#for i in range(0, len(operations)):
+	#	features['previous2Word:'+str(previous2words[i+1])+"_operation:"+operations[i]] += 1	
+
+	#next2words = problem['next2words']
+	#features['next2Word:'+str(next2words[0])+"_operation:+"] += 1
+	#for i in range(0, len(operations)):
+	#	features['next2Word:'+str(next2words[i+1])+"_operation:"+operations[i]] += 1	
+
 	# Feature 4: operation:op_positionInTemplate:pos
 	for i in range(0, len(operations)):
 		features['operation:'+operations[i]+"_positionInTemplate:"+str(i+1)] += 1
 	#features['operation:'+operations[1]+"_positionInTemplate:2"] += 1
 
 	# Feature 5: secondOperation in template
-	if len(operations) > 1:
-		features['secondOperation:'+operations[1]] += 1
-	if len(operations) > 2:
-		features['thirdOperation:'+operations[2]] += 1
-	if len(operations) > 3:
-		features['fourthOperation:'+operations[3]] += 1
+	#if len(operations) > 1:
+	#	features['secondOperation:'+operations[1]] += 1
+	#if len(operations) > 2:
+	#	features['thirdOperation:'+operations[2]] += 1
+	#if len(operations) > 3:
+	#	features['fourthOperation:'+operations[3]] += 1
 
 	# Feature 6: asksHowMany:True/False_resultType:int/float
 	combination_result = eval(combination)
@@ -470,7 +503,8 @@ def argmax(problem, weights, dataset_name):
 	return max_dot, max_features, max_combination
 
 # Train the strucutred perceptron and learn the weights. 
-# Multiple passes, shuffline and averaging can improve the performance of our classifier. 
+# 1) Multiple passes, shuffling and averaging can improve the performance of our classifier. 
+# 2) Online passive-aggressive
 def train(data, dataset_name, iterations=5,  debugging=False):
 	# Initialise the weights
 	weights = initialise_weights(data)
@@ -490,9 +524,65 @@ def train(data, dataset_name, iterations=5,  debugging=False):
 			# Predict the sequence of numbers and operations according to the template. 
 			y_hat_dot, y_hat_features, y_hat_combination = argmax(data[problem], weights, dataset_name)
 
+
+			'''
+			##### Passive-Aggressive ##### -> FAILED!!!
+			# Set aggressive parameter
+			C = 0.1
+
+			# Compute suffer loss
+			prediction = eval(y_hat_combination)
+			solution = data[problem]['solution']
+
+			
+			features_diff = Counter()
+
+			for feature in features.keys():
+				if feature in y_hat_features.keys():
+					features_diff[feature] = features[feature] - y_hat_features[feature]
+				else:
+					features_diff[feature] = features[feature]
+
+			for feature in y_hat_features.keys():
+				if feature not in features.keys():
+					features_diff[feature] = -features[feature]
+
+			r = 0
+			for feature in features.keys():
+				if feature not in y_hat_features.keys():
+					r += 1
+			for feature in y_hat_features.keys():
+				if feature not in features.keys():
+					r += 1
+
+			loss_t = dot_product(y_hat_features, weights) - dot_product(features, weights) + math.sqrt(r)
+
+			vector_features_diff = list(features_diff.values())
+
+			denominator = numpy.linalg.norm(vector_features_diff) ** 2
+			if denominator == 0:
+				t = 0
+			else:
+				#t = min(C, loss_t / denominator)
+				t = loss_t / denominator
+
+			# Update weights
+			for feature in features_diff.keys():
+				if feature in weights.keys():
+					w = weights[feature]
+					weights[feature] += t
+
+			#for feature in weights.keys():
+			#	if feature in features_diff.keys():
+			#		weights[feature] += t*weights[feature]
+			#sys.exit(1)
+			
+			##### Passive-Aggressive #####
+			'''
+
 			# Extract the correct/wrong features
-			features_for_addition = []
-			features_for_subtraction = []
+			features_for_addition = [] # correct features
+			features_for_subtraction = [] # wrong features
 
 			for feature in features:
 				if feature not in y_hat_features.keys():
@@ -501,6 +591,7 @@ def train(data, dataset_name, iterations=5,  debugging=False):
 			for feature in y_hat_features.keys():
 				if feature not in features.keys() or feature not in weights.keys():
 					features_for_subtraction.append(feature)
+
 
 			# Update the weights according to the prediction
 			for feature in features_for_addition:
@@ -515,11 +606,11 @@ def train(data, dataset_name, iterations=5,  debugging=False):
 
 			if solution == prediction:
 				training_accuracy += 1
- 
+
+
 		# Averaging weights
 		for key, value in weights.items():
 			averaged_weights[key] += value
-
 
 		# Compute the training accuracy for each iteration
 		accuracy = training_accuracy / len(data) * 100
@@ -528,18 +619,33 @@ def train(data, dataset_name, iterations=5,  debugging=False):
 		for key, value in averaged_weights.items():
 			averaged_weights[key] = value / iterations
 
-	return averaged_weights, training_accuracies
+	####### DON'T FORGT TO CHANGE weights TO averaged_weights
+	return weights, training_accuracies
 
 # Testing phase of structured perceptron
 def test(data, weights, dataset_name, debugging=False):
 	correct_counter = 0
 	incorrect_problems = [] # error analysis
+	incorrect_counter = 0
 
+
+	allarith_error_counter_1 = 0
+	allarith_error_counter_2 = 0
+	allarith_error_counter_3 = 0
+	allarith_error_counter_4 = 0
+	allarith_error_counter_5 = 0
+
+
+
+	error_analysis = {}
 	for problem in data:
 		features = data[problem]['features']
 		solution = data[problem]['solution']
 		question = data[problem]['question']
 		equation = data[problem]['equation']
+
+		if dataset_name == "AllArith":
+			quantities = data[problem]['quantities']
 
 		# Predict the sequence of numbers and operations according to the template. 
 		y_hat_dot, y_hat_features, y_hat_combination = argmax(data[problem], weights, dataset_name)	
@@ -547,22 +653,62 @@ def test(data, weights, dataset_name, debugging=False):
 		# Execute the predicted equation
 		prediction = eval(y_hat_combination)
 
-		if solution == prediction:
+		if round(solution, 10) == round(prediction, 10): # round(): deal with issues caused by floating type
 			correct_counter += 1
 		else:
+			error_analysis[incorrect_counter] = {}
+			error_analysis[incorrect_counter]['question'] = question
+			error_analysis[incorrect_counter]['equation'] = equation
+			error_analysis[incorrect_counter]['predicted'] = y_hat_combination 
+			error_analysis[incorrect_counter]['solution'] = solution
 			incorrect_problems.append(question)
+			incorrect_counter += 1
+
+
+			if dataset_name == "AllArith":
+				if len(quantities) == 2:
+					allarith_error_counter_1 += 1
+				elif len(quantities) == 3:
+					allarith_error_counter_2 += 1
+				elif len(quantities) == 4:
+					allarith_error_counter_3 += 1
+				elif len(quantities) == 5:
+					allarith_error_counter_4 += 1
+				elif len(quantities) == 6:
+					allarith_error_counter_5 += 1
+
 
 	accuracy = correct_counter / len(data) * 100
+
+	#print("\nMisclassified examples:")
 	#print(incorrect_problems)
+	#for p in error_analysis.keys():
+	#	print("\nQuestion: " + error_analysis[p]['question'])
+	#	print("Equation: " + error_analysis[p]['equation'])
+	#	print("Predicted: " + error_analysis[p]['predicted'])
+	#	print("Executed: " + str(eval(error_analysis[p]['predicted'])))
+	#	print("Solution:" + str(error_analysis[p]['solution']))
+	#	print('\n')
+	#print("\n")
+
+	#if dataset_name == "AllArith":
+	#	print("\n Incorrect predictions with 1 op: " + str(allarith_error_counter_1))
+	#	print("Incorrect predictions with 2 op: " + str(allarith_error_counter_2))
+	#	print("Incorrect predictions with 3 op: " + str(allarith_error_counter_3))
+	#	print("Incorrect predictions with 4 op: " + str(allarith_error_counter_4))
+	#	print("Incorrect predictions with 5 op: " + str(allarith_error_counter_5))
+
 	return accuracy
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
+	# Start time
+	start = time.time()
+
 	# Fix random seed so that results are reproducible. 
-	random.seed(26)
- 
-	dataset_name = handle_command_line()
+	random.seed(11) # or 146
 
 	# Read the data
+	dataset_name = handle_command_line()
 	data = read_data_json('data/'+dataset_name+'.json')
 
 	# Report dataset's stats -> stdout
@@ -573,30 +719,36 @@ if __name__ == "__main__":
 
 	accuracy_per_fold = []
 	accuracy = 0 
+	training_accuracies_per_fold = []
 	for i in range(0, len(k_folds)):
 		print("Fold "+ str(i+1) + "/"+str(len(k_folds)))
 		training_indices = k_folds[i][0]
 		testing_indices = k_folds[i][1]
 
 		# Extract features for training and testing examples
-		if dataset_name == "AllArith":
+		if dataset_name.startswith("AllArith"):
 			training_features = AllArithDataset.extract_features(data, training_indices)
 			testing_features = AllArithDataset.extract_features(data, testing_indices)
 		else:
 			training_features = extract_features(data, training_indices)
 			testing_features = extract_features(data, testing_indices)
 
-
 		# Train the structured perceptron in order to learn the weights
 		feature_weights, training_accuracy = train(training_features, dataset_name)
-
-		#sys.exit(1)
+		training_accuracies_per_fold.append(training_accuracy)
 
 		# Test the perceptron
 		curr_accuracy = test(testing_features, feature_weights, dataset_name)
 		accuracy_per_fold.append(curr_accuracy)
 		accuracy += curr_accuracy 
 
-	print("Testing accuracy: " + str(accuracy / len(accuracy_per_fold)))
-	print("Accuracy per fold: " + str(accuracy_per_fold))
-	print("Training accuracy per iteration: " + str(training_accuracy))
+	print("\nTesting accuracy: " + str(accuracy / len(accuracy_per_fold)))
+	print("Testing cccuracy per fold: " + str(accuracy_per_fold))
+
+	print("\nTraining accuracy per fold:")
+	for i in range(0, len(training_accuracies_per_fold)):
+		print("Fold " + str(i) + ": " + str(training_accuracies_per_fold[i]))
+
+	# Report execution time. 
+	end = time.time()
+	print("\nExecution time: " + str(end - start))
